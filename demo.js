09c396e5817f89753b80dc7056b07be665ec9928
kk <359389528@qq.com>
2022-10-26 08:43:49 +0800

    第一次提交

---
+++ demo.js
@@ -0,0 +1,78 @@
+//class继承
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+	say() {
+		console.log(this.name, this.age);
+	}
+}
+
+class Student extends Person {
+	constructor(name, age, score) {
+		super(name, age);
+		this.score = score;
+	}
+	say() {
+		super.say();
+		console.log(this.score);
+	}
+}
+
+let obj = new Student('kk', '30', 100);
+
+obj.say();
+
+// Object.keys(obj);
+//proxy 拦截  数组  set
+
+// Object.defineProperty(obj,name,{
+//      get(){
+//         console.log('调用get')
+//     }
+//     set(){
+//         console.log('调用set')
+//     },
+
+// })
+
+var obj = {
+	name: 'KK',
+	age: '30'
+};
+let proxy = new Proxy(obj, {
+	set(target, key, value) {
+		console.log('调用get', target, key, value);
+		target[key] = value;
+	},
+	get(target, key) {
+		console.log('调用get', target, key);
+		return target[key];
+	}
+});
+
+//数组去重
+let arr = [1, 2, 3, 1, 2, 8, 9];
+let sarr = new Set(arr);
+let newarr = [...sarr];
+
+let arr1 = new Set([1, 2, 3, 4, 5]);
+let arr2 = new Set([2, 5, 6, 7, 8]);
+//数组取并集
+let symbolarr = new Set([...arr1, ...arr2]);
+//数组取交集
+let newarr3 = new Set([...arr1].filter((item) => arr2.has(item)));
+//数组取差集
+let newarr4 = new Set([...arr1].filter((item) => !arr2.has(item)));
+
+//Symbol()
+
+let keys = {
+	name: Symbol("name"),
+	age: Symbol("age")
+};
+let sobj = {
+	[keys.name]: 'kk',
+	[keys.age]: '30'
+};
---
+++ vite-project/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+	<div id="myChart" style="width: 500px; height: 300px; margin: auto"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+import { onMounted } from 'vue';
+
+export default {
+	setup() {
+		onMounted(() => {
+			let myChart = echarts.init(document.getElementById('myChart'),"");
+			// 设置图表配置项,可以直接从Echarts的示例中，将配置项复制下来后放入下列的myChart.setOption()中实现图表的样式更换
+			myChart.setOption({
+				title: {
+					text: 'ECharts'
+				},
+				tooltip: {},
+				xAxis: {
+                    type:"category",
+					data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+				},
+				yAxis: {},
+				series: [
+					{
+						name: '销量',
+						type: 'line',
+                        smooth:true,
+						data: [5, 20, 36, 10, 10, 20]
+					},
+					
+				]
+			});
+			window.onresize = function () {
+				// 自适应大小
+				myChart.resize();
+			};
+		});
+	}
+};
+</script>
